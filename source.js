8889149410ab14fc4dc75a393eea79639d8871d5
Jae Kwon <jkwon.work@gmail.com>
2011-10-22 21:57:47 -0700

    .

---
+++ source.js
@@ -0,0 +1,59 @@
+// Comment Collapse Bookmarklet
+// Jae Kwon 2011
+// MIT License
+
+(function() {
+
+function collapse(cells, startIndex, atIndent) {
+  var thisCell = jQuery(cells[startIndex]);
+  var children = jQuery();
+  var numDescendants = 0;
+  for (var i=startIndex; i<cells.length; i++) {
+    var cell = jQuery(cells[i]);
+    if (cell.data('indent') == atIndent) {
+      cell.find('td:eq(2)>span.comment>p:eq(1)>font').append(" ", collapse(cells, i+1, atIndent +1));
+      children = children.add(cell);
+    } else if (cell.data('indent') < atIndent) {
+      break;
+    }
+    numDescendants += 1;
+  }
+  if (children.length > 0 && atIndent > 0) {
+    children.hide();
+    if (children.length < numDescendants) {
+      var expand = jQuery('<a href="#">comments ('+children.length+'/'+numDescendants+')</a>');
+    } else {
+    var expand = jQuery('<a href="#">comments ('+children.length+')</a>');
+    }
+    expand.click(function(e) {
+      expand.remove();
+      children.show();
+      return false;
+    });
+    return expand;
+  } else {
+    return null;
+  }
+}
+
+
+function onLoad() {
+  var cells = jQuery('table:eq(3)>tbody>tr>td>table>tbody>tr');
+  cells.each(function(i, cell) {
+    var width_px = jQuery(cell).find('>td>img').css('width');
+    var width = Number(width_px.substr(0,width_px.length-2));
+    var indent = width/40;
+    jQuery(cell).data('indent', indent);
+  });
+  jQuery(document.body).append(collapse(cells, 0, 0));
+}
+
+
+var headID = document.getElementsByTagName("head")[0];
+var newScript = document.createElement('script');
+newScript.type = 'text/javascript';
+newScript.onload=function() { onLoad(); };
+newScript.src = '//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js';
+headID.appendChild(newScript);
+
+})();
